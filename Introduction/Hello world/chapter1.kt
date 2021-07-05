dc1dcaba2595c9b0bdc448e3b813f98b78fadec4
TimzOwen <ktimothy@kabarak.ac.ke>
2021-07-05 09:08:24 +0300

    null point exception

--- Introduction/Hello world/chapter1.kt
+++ Introduction/Hello world/chapter1.kt
@@ -10,4 +10,62 @@ printHello()
 
 // Operators (+-/)
 
+1 + 1 = 2   // addition
+5 - 2 = 3   //subtraction
+50 / 10 = 5 // whole number division
+1 / 2 = 0   // division of whole numbers
+1.0 / 2.5 = 0.5 //division of fractions
+6 * 5 = 30  // multiplication
+
+val fish3 = 4
+val fish4 = 5
+
+fish4 + fish3
+
+
+// operation methods
+// call methods on values
+ val fish = 5
+ fish.minus(2)   //subtraction
+ fish.plus(4)    // addition
+ fish.times(2)   // multiplication
+ fish.div(3) // divison
+res0: kotlin.Int = 1
+
+
+//Variables
+
+val fish1 = 20   // unchangable
+var fish2 = 10   // changeable
+
+
+// implicit $ explicit casting
+
+//wrong casting approach
+val b: Byte = 1
+val i: int = b  // ERROR!!!!!!
+
+//Right casting approach
+val int i: Int = b.toInt()      
+
+
+// LONG VARIBALE NUMBER UNDERSCORE
+val oneMillion = 1_000_000
+val socialNum = 234_456_345_764
+
+
+// NULL VALUES KOTLIN
+//values cannot be null in kotlin. use question marks
+
+// ERROR!!! ---> Cannot be null error
+var rocks: Int = null 
+
+//use question marks 
+var marbles: Int? = null
+var numOfFish: List<Strings?> = listof(null, null)  // null list
+var moreFish : List<String>? = null // elements
+var moreMoreFish List<String?>?  = null // both List and elements are null
+
+
+
 
