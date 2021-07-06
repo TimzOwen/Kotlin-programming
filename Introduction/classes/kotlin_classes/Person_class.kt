2568486700979f4f2235530d4d5871ec0aa9eb08
TimzOwen <ktimothy@kabarak.ac.ke>
2021-07-06 11:00:44 +0300

    updated getter and setter from default values

--- Introduction/classes/kotlin_classes/Person_class.kt
+++ Introduction/classes/kotlin_classes/Person_class.kt
@@ -17,7 +17,94 @@ class Person(_fName:String , _sName : String){
     val secondName : String = _sName
     
     }
+}
 
+
+
+//class (002)
+
+// direct initialisation from primary constructor
+
+class Person(val fName:String , val sName : String){
+
+}
+
+// CLASS (003)
+//CLASS (003)
+//SECONDARY CONSTRUCTOR
+
+// direct initialisation from constructor
+class Person(val fName:String , val sName : String){
+
+    init {
+        println(" init one")
+    }
+
+    // secondary constructor --> : -> calls primary constructor
     
+    constructor() : this("Timz","Owen"){
+        println("secondary constructor")
+    }
+
+    init {
+        println("init two")
+    }
+
+
+//CLASS (004)
+//Default constructors
+
+// direct initialisation from constructor
+class Person(val fName:String="Code" , val sName : String="Ninja")
+
+
+
+//CLASS (005)
+//class properties (getter and setter for not default values)
+
+// direct initialisation from constructor
+class Person(val fName:String="Code" , val sName : String="Ninja"){
+
+    var nickName : String? = null
+        //setter
+        set(value) {
+            field = value
+            println("new name is $nickName")
+        }
+        //getter
+        get() {
+            println("returned value is $field")
+            return field
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- Introduction/classes/kotlin_classes/main_class.kt
+++ Introduction/classes/kotlin_classes/main_class.kt
@@ -4,3 +4,74 @@ fun main() {
     // create an instance of the main class
     val person = Person("Timz","Owen")
 }
+
+
+// Classes (002)
+fun main() {
+
+    // create an instance of the main class
+    val person = Person("Timz","Owen")
+
+    // accessing the properties of a class
+    println(person.fName)
+    println(person.sName)
+}
+
+
+// CLASS (003)
+
+// SECONDARY CONSTRUCTOR
+
+fun main() {
+    // create an instance of the main class
+    val person = Person("Timz","Owen")
+
+    // accessing the properties of a class
+    // n/b -> secondary constructor is never called
+    println(person.fName)
+    println(person.sName)
+
+
+    // task 2 - > remove the default parameter and check sec get executed
+    val person2 = Person()
+    println(person2.fName)
+    println(person2.sName)
+
+
+}
+
+
+// CLASS (004)
+// Default value & default constructor
+
+fun main() {
+    // create an instance of the main class
+    val person = Person()
+
+    // accessing the properties of a class
+    println(person.fName)
+    println(person.sName)
+
+
+}
+
+
+// CLASS (005)
+// class properties
+
+fun main() {
+    // create an instance of the main class
+    val person = Person()
+
+    // accessing the properties of a class
+    println(person.fName)
+    println(person.sName)
+    person.nickName = "producer"
+    person.nickName = "engineer"
+
+
+}
+
+
+
+// 
