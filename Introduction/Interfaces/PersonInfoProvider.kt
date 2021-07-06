d82afc9c87d64a45eb543982b3335d9735b0a273
TimzOwen <ktimothy@kabarak.ac.ke>
2021-07-06 12:37:20 +0300

    updated interface class

---
+++ Introduction/Interfaces/PersonInfoProvider.kt
@@ -0,0 +1,19 @@
+// interface(001)
+
+//abstract class--> It doesn't need to implement all the methods of a class
+
+interface PersonInfoProvider{
+    fun printInfo(person : Person)
+}
+
+// " : " ---> used to show implementation
+
+class BasicProviderInfo : PersonInfoProvider{
+    override fun printInfo(person: Person) {
+        TODO("Not yet implemented")
+    }    
+}
+
+fun main() {
+    val provider = BasicProviderInfo()
+}
