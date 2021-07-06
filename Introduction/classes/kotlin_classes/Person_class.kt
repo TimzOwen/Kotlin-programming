9064555b070eabe4e9b229b39fb79d722ad505e6
TimzOwen <ktimothy@kabarak.ac.ke>
2021-07-06 10:18:11 +0300

    updated classes

--- Introduction/classes/kotlin_classes/Person_class.kt
+++ Introduction/classes/kotlin_classes/Person_class.kt
@@ -1,1 +1,23 @@
 
+class Person(_fName:String , _sName : String){
+    val firsName : String
+    val secondName : String
+
+    // initialize the values
+    init {
+        firsName = _fName
+        secondName = _sName
+    }
+
+
+
+    // can be simplified to 
+    class Person(_fName:String , _sName : String){
+    val firsName : String = _fName
+    val secondName : String = _sName
+    
+    }
+
+    
+
+}
--- Introduction/classes/kotlin_classes/main_class.kt
+++ Introduction/classes/kotlin_classes/main_class.kt
@@ -1,1 +1,6 @@
 
+// Classes
+fun main() {
+    // create an instance of the main class
+    val person = Person("Timz","Owen")
+}
