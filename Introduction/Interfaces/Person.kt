4401e44bdfdcd9a39e6ec39a863d45785d21c644
Timz Owen <ktimothy@kabarak.ac.ke>
2021-07-06 12:38:51 +0300

    Create Person.kt
    persons and interface class implementation

---
+++ Introduction/Interfaces/Person.kt
@@ -0,0 +1,28 @@
+//Interface (001)
+//class modifiers
+
+// direct initialisation from constructor
+class Person(val fName:String="Code" , val sName : String="Ninja"){
+
+    // protected --> only available within the class or sub class not outside the file
+
+    var nickName : String? = null
+        //setter
+        set(value) {
+            field = value
+            println("new name is $nickName")
+        }
+        //getter
+        get() {
+            println("returned value is $field")
+            return field
+        }
+
+    // private / protected method will only be accessed here within the file
+
+    fun printUserInfo(){
+        //create a var to check for null value (use office operator)
+        var nickNameToPrint = nickName ?: "No nickName"
+        println("Hello $fName ($nickNameToPrint) $sName")
+    }
+}
