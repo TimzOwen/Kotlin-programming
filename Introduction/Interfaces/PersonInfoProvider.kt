4fdb4cce636e3ed84330a2ee379ba436b44643db
TimzOwen <ktimothy@kabarak.ac.ke>
2021-07-07 14:14:45 +0300

    updated caller

--- Introduction/Interfaces/PersonInfoProvider.kt
+++ Introduction/Interfaces/PersonInfoProvider.kt
@@ -17,3 +17,27 @@ class BasicProviderInfo : PersonInfoProvider{
 fun main() {
     val provider = BasicProviderInfo()
 }
+
+// interface(002)
+
+//abstract class--> It doesn't need to implement all the methods of a class
+
+interface PersonInfoProvider{
+    fun printInfo(person : Person)
+}
+
+// " : " ---> used to show implementation
+class BasicProviderInfo : PersonInfoProvider{
+    override fun printInfo(person: Person) {
+        println("print info")
+        person.printUserInfo()
+    }
+}
+
+fun main() {
+    val provider = BasicProviderInfo()
+
+    provider.printInfo(Person())
+}
+
+
