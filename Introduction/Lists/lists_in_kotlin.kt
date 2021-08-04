bc65b3ab228f6b0e0223fdae3d3d99e6573fd094
Timz Owen <ktimothy@kabarak.ac.ke>
2021-08-04 15:48:16 +0300

    Update lists_in_kotlin.kt
    generic types in List  in kotlin

--- Introduction/Lists/lists_in_kotlin.kt
+++ Introduction/Lists/lists_in_kotlin.kt
@@ -28,3 +28,32 @@ fun main(){
     println("reversed list in ${fruits.reversed()}")
     println("ordered elements ${fruits.sorted()}")
 }
+
+
+
+
+// Genericc types in kotlin
+
+// introduction to mutalble list(specify the Generic type)
+fun main(){
+    val names : MutableList<String> = mutableListOf()
+    
+    println("added Timz here as ${names.add("Timz Owen")}")
+    println(names)	// displays a new name here
+    
+    // adding more than one element (addAll())
+    val otherNames = listOf("Shem","Salma","Timothy","Chelsea")
+    println("added and its ${names.addAll(otherNames)} to $names")
+    
+    // Remove elements from the list
+    println("${names.remove("Salma")} and becomes ${names}")
+    
+    // Remove at specific index
+    println("removed index 1 ${names.removeAt(0)} and becomes $names")
+    
+    // clear all elements from the list
+    println("And there are no elements ${names.clear()}") // no element in kotloin unit
+    
+}
+
+
