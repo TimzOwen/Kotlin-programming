aa200845c991fd47bb8aa4e29756c35f765d8f15
Timz Owen <ktimothy@kabarak.ac.ke>
2021-08-17 15:03:13 +0300

    Update classes.kt
    Updated constrictor for roundTower to increase with increasing floor and added default floor

--- Introduction/classes part 2/classes.kt
+++ Introduction/classes part 2/classes.kt
@@ -103,3 +103,64 @@ fun main(){
     }
     
 }
+
+
+
+
+
+
+
+// EDITABLE CAPACITY
+
+
+// classes and inheritance 
+
+abstract class Dwelling(private var residents : Int){
+    abstract val buildingMaterial : String
+    abstract val capacity : Int
+    
+    fun hasRoom() : Boolean {
+        return residents < capacity
+    }   
+}
+
+class SquareCabine(residents : Int) : Dwelling(residents){
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
+}
+open class RoundHat(residents : Int) : Dwelling(residents){
+    override val buildingMaterial = "Straw"
+    override val capacity = 10
+}
+
+class RoundTower(residents : Int,
+                val floors : Int = 3) : RoundHat(residents){
+    override val buildingMaterial = "stone"
+    override val capacity = 10 * floors
+}
+
+fun main(){
+    val squareCab = SquareCabine(8)
+    with(squareCab){
+        println("Sqaure cabin")
+        println("has room is: ${hasRoom()}")
+        println("Capacity is: ${capacity}")
+        println("Material made is : ${buildingMaterial}")
+    }
+    val roundHat = RoundHat(8)
+    with(roundHat){
+        println("\nRoundHat==")
+        println("HasRoom : ${hasRoom()}")
+        println("Capacity is: ${capacity}")
+        println("Material is: ${buildingMaterial}")
+    }
+    
+    val roundTower = RoundTower(5,4)
+    with(roundTower){
+        println("\nRoundTower==")
+        println("HasRoom : ${hasRoom()}")
+        println("Capacity is: ${capacity}")
+        println("Material is: ${buildingMaterial}")        
+    }
+    
+}
