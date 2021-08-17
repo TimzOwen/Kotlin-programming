4a3fd006288269486eeb6600582897a890d3e507
Timz Owen <ktimothy@kabarak.ac.ke>
2021-08-17 14:29:39 +0300

    Update classes.kt
    Kotlin subclass

--- Introduction/classes part 2/classes.kt
+++ Introduction/classes part 2/classes.kt
@@ -59,3 +59,47 @@ fun main(){
 }
 
 
+
+
+
+
+
+// ADDING CLASS SECOND CLASS
+
+// classes and inheritance 
+
+abstract class Dwelling(private var residents : Int){
+    abstract val buildingMaterial : String
+    abstract val capacity : Int
+    
+    fun hasRoom() : Boolean {
+        return residents < capacity
+    }   
+}
+
+class SquareCabine(residents : Int) : Dwelling(residents){
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
+}
+class RoundHat(residents : Int) : Dwelling(residents){
+    override val buildingMaterial = "Straw"
+    override val capacity = 10
+}
+
+fun main(){
+    val squareCab = SquareCabine(8)
+    with(squareCab){
+        println("Sqaure cabin")
+        println("has room is: ${hasRoom()}")
+        println("Capacity is: ${capacity}")
+        println("Material made is : ${buildingMaterial}")
+    }
+    val roundHat = RoundHat(8)
+    with(roundHat){
+        println("\nRoundHat==")
+        println("HasRoom : ${hasRoom()}")
+        println("Capacity is: ${capacity}")
+        println("Material is: ${buildingMaterial}")
+    }
+    
+}
