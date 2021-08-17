200a951986d068ca1381e395a1fc80ae47b9f64c
Timz Owen <ktimothy@kabarak.ac.ke>
2021-08-17 14:13:45 +0300

    Update classes.kt
    class inheritance update

--- Introduction/classes part 2/classes.kt
+++ Introduction/classes part 2/classes.kt
@@ -1,4 +1,29 @@
 
 // Classes and inheritance
 
+// classes and inheritance 
+
+abstract class Dwelling(private var residents : Int){
+    abstract val buildingMaterial : String
+    abstract val capacity : Int
+    
+    fun hasRoom() : Boolean {
+        return residents < capacity
+    }   
+}
+
+class SquareCabine(residents : Int) : Dwelling(residents){
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
+}
+
+fun main(){
+    val squareCab = SquareCabine(4)
+    println("Sqaure cabin")
+    println("has room is: ${squareCab.hasRoom()}")
+    println("Capacity is: ${squareCab.capacity}")
+    println("Material made is : ${squareCab.buildingMaterial}")
+    
+}
+
 
