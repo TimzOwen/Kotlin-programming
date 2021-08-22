af1cba1491a2e0b0c77a088c7594d4346e470ca3
Timz Owen <ktimothy@kabarak.ac.ke>
2021-08-22 20:59:25 +0300

    Update collections.kt
    kotlin collections

--- Introduction/collections/collections.kt
+++ Introduction/collections/collections.kt
@@ -1,1 +1,12 @@
 
+//collections in Kotlin
+
+// collection are list of items of the same types
+
+fun main() {
+    val numbers = listOf(2,2,4,4,6,8,10,0,3,4)
+    println("list: ${numbers}")
+    println("sorted: ${numbers.sorted()}")
+    println("Set: No repeat: ${numbers.toSet()}") // elements don't repeat
+}
+
