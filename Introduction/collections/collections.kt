1bfd13d345b7176704106d452da18a4853feef88
Timz Owen <ktimothy@kabarak.ac.ke>
2021-08-22 21:45:56 +0300

    Update collections.kt
    maps and function

--- Introduction/collections/collections.kt
+++ Introduction/collections/collections.kt
@@ -10,3 +10,36 @@ fun main() {
     println("Set: No repeat: ${numbers.toSet()}") // elements don't repeat
 }
 
+
+
+
+
+// Maps --> key value pair data storage.
+
+// values can be --> objects, numbers or strings.
+
+fun main() {
+    val people = mutableMapOf<String, Int>(
+    "Timz" to 30,
+    "Chelsea" to 60)
+    people.put("Gildred" , 70) // add new member to a map
+    people["Doe"] = 45			// Add new members 
+    people["Timz"] = 100	// modifying values if existing (not added)
+    println(people)
+}
+
+
+// LOOPING THROUGH MAPS
+
+fun main() {
+    val people = mutableMapOf<String, Int>(
+    "Timz" to 30,
+    "Chelsea" to 60)
+    people.put("Gildred" , 70) 
+    people["Doe"] = 45	
+    people["Timz"] = 100	
+    
+    //iterate through the map
+    people.forEach{println("${it.key} is ${it.value}")}
+}
+
